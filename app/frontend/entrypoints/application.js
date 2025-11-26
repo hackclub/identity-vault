8dd45c81d6585114ff42b8c503be57e116fe0dfe
Mahad Kalam <mahadkalam1234@gmail.com>
2025-11-26 23:23:50 +0000

    More updates

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -2,13 +2,8 @@ import "../js/alpine.js";
 import "../js/lightswitch.js";
 import "../js/click-to-copy";
 import "../js/otp-input.js";
-import { registerWebauthn } from "../js/webauthn-registration.js";
-import { authenticateWebauthn } from "../js/webauthn-authentication.js";
 import htmx from "htmx.org"
 window.htmx = htmx
-
-window.registerWebauthn = registerWebauthn;
-window.authenticateWebauthn = authenticateWebauthn;
 
 // Add CSRF token to all HTMX requests
 document.addEventListener('htmx:configRequest', (event) => {
@@ -16,14 +11,4 @@ document.addEventListener('htmx:configRequest', (event) => {
   if (csrfToken) {
     event.detail.headers['X-CSRF-Token'] = csrfToken;
   }
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  registerWebauthn.init();
-  authenticateWebauthn.init();
-});
-
-document.addEventListener('htmx:afterSwap', () => {
-  registerWebauthn.init();
-  authenticateWebauthn.init();
 });
